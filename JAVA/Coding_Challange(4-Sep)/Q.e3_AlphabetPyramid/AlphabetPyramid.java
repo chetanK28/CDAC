f9d6dc51599210a43b468d4eb8aa3d6ce3397bfe
Chetan Arun Kirange <137753681+chetanK28@users.noreply.github.com>
2024-09-04 18:02:32 +0530

    Update AlphabetPyramid.java

--- JAVA/Coding_Challange(4-Sep)/Q.e3_AlphabetPyramid/AlphabetPyramid.java
+++ JAVA/Coding_Challange(4-Sep)/Q.e3_AlphabetPyramid/AlphabetPyramid.java
@@ -1,1 +1,21 @@
+public class AlphabetPyramid {
+    public static void main(String[] args) {
+        int n = 5; // rows for pyramid
+        char alphabet = 'A'; 
+        
+        for (int i = 0; i < n; i++) { 
+            for (int j = i; j < n - 1; j++) {
+                System.out.print(" ");
+            }
+            
+            for (int j = 0; j < (2 * i + 1); j++) {
+                System.out.print(alphabet);
+                alphabet++; 
+            }
+            
+            System.out.println();
+        }
+    }
+    
+}
 
