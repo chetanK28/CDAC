d65ece7ef58505ce13104cfd791c79e98ad156f4
Chetan Arun Kirange <137753681+chetanK28@users.noreply.github.com>
2024-09-06 12:07:34 +0530

    Update employee.java

--- JAVA/Daily updates/employee.java
+++ JAVA/Daily updates/employee.java
@@ -1,1 +1,35 @@
+import java.util.Scanner;
 
+public class Employee {
+    int empid;
+    String name;
+    String City;
+    float Salary;
+
+    public void getdata() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the empid: ");
+        empid = sc.nextInt();
+        sc.nextLine(); 
+        System.out.println("Enter the name: ");
+        name = sc.nextLine();
+        System.out.println("Enter the City:");
+        City = sc.nextLine();
+        System.out.println("Enter the Salary:");
+        Salary = sc.nextFloat();
+        sc.close();
+    }
+
+    public void putdata() {
+        System.out.println("Employee id is: " + empid);
+        System.out.println("Name is: " + name);
+        System.out.println("City is:" +City);
+        System.out.println("Salary is: " + Salary);
+    }
+
+    public static void main(String args[]) {
+        Employee emp = new Employee();
+        emp.getdata();
+        emp.putdata();
+    }
+}
